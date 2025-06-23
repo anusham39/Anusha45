b4a43feadf877ecd8e3293b0e0a0d72d7f6756e7
anusham39@gmail.com <anusham39@gmail.com>
2025-06-23 14:03:10 +0530

    Commited on exam 23-6-2025

---
+++ src/exam_Test236/Exam1_Test2.java
@@ -0,0 +1,22 @@
+package exam_Test236;
+
+import java.util.Scanner;
+
+public class Exam1_Test2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        int a = scanner.nextInt();
+        System.out.println("Enter second number");
+        int b = scanner.nextInt();
+        System.out.println("Addition: " +(a+b));
+        System.out.println("subtraction: " +(a-b));
+        System.out.println("Multiplication: "+(a*b));
+        System.out.println("Divison: " +(a/b));
+        System.out.println("Modulus: " +(a%b));
+
+
+
+
+    }
+}
