b8a6b7beb33b68b3933b1962f058a28d0c2629f7
anusham39@gmail.com <anusham39@gmail.com>
2025-06-23 14:37:59 +0530

    Commited on exam 23-6-2025

---
+++ src/exam_Test236/Exam1_Test7.java
@@ -0,0 +1,14 @@
+package exam_Test236;
+
+public class Exam1_Test7 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 27;
+        int c = 19;
+        if((b>a) &&(b>c))
+            System.out.println("Largest number is: " +b);
+        int i= 10, j= 10, k = 8;
+        if((i == j) &&(i > k))
+            System.out.println("Largest number is:" +i);
+    }
+}
