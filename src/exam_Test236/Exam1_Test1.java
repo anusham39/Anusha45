ec2600b3acb20fc83c75aade9298af40f4cf1de5
anusham39@gmail.com <anusham39@gmail.com>
2025-06-23 13:48:56 +0530

    Commited on exam 23-6-2025

---
+++ src/exam_Test236/Exam1_Test1.java
@@ -0,0 +1,15 @@
+package exam_Test236;
+
+public class Exam1_Test1 {
+    public static void main(String[] args) {
+        int a = 25;
+        double pi = 3.14;
+        boolean result = true;
+        char a1 = 'A';
+        System.out.println("Integer: " +a);
+        System.out.println("Double: " +pi);
+        System.out.println("Boolean: "+result);
+        System.out.println("Char: " +a1);
+
+    }
+}
